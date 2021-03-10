ad3d9f823a918020276f40f99159ba4aec182cf6
Jeweled Fox <79212970+jeweled-fox@users.noreply.github.com>
2021-03-10 10:16:17 -0500

    Create hacks.js
    the hacks to use.

---
+++ hacks.js
@@ -0,0 +1,21 @@
+//This is where the cheats are labeled and stored. Anytime you want to use them, use inspect element, click "console", copy/paste the command that you want to use, and reload the page.
+
+localStorage.setItem("coinsOwned", coinsyouwant); //Replace "coinsyouwant" with the amount that you want.
+
+localStorage.setItem("playerXP", xpyouwant); //Replace "xpyouwant" with the amount that you want.
+
+localStorage.setItem("playerSkin", skinyouwant); //Replace "skinyouwant" with the skin that you want, a number ranging from 1 to 35.
+
+localStorage.setItem("playerPet", petyouwant); //Replace "petyouwant" with the pet that you want, a number ranging from 1 to 9.
+
+localStorage.setItem("playerPetLevel", levelyouwant); //Replace "levelyouwant" with the pet level that you want, a number ranging from 1 to 14.
+
+//These are the required xp levels that are needed to unlock certain islands. Use these with the playerXP command above.
+
+//0 - Tortuga
+//140 - Beach
+//700 - Easter
+//2100 - Wreck
+//4400 - Aztec
+//7600 - Volcano
+//13500 - Village

df6beb237198ca63bcd60fc84e7bd65e7d851a66
Julian <79212970+jweled@users.noreply.github.com>
2023-09-05 15:58:17 -0400

    undefeated template
    https://github.com/jweled/yohoho-hack/pull/1

---
+++ undefeated.js
@@ -0,0 +1,23 @@
+//from deeznanas1234's PR: https://github.com/jweled/yohoho-hack/pull/1
+
+//This is where the cheats are labeled and stored. Anytime you want to use them, use inspect element, click "console", copy/paste the command that you want to use, and reload the page.
+
+localStorage.setItem("coinsOwned", 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999); //Replace "coinsyouwant" with the amount that you want.
+
+localStorage.setItem("playerXP", 0); //Replace "xpyouwant" with the amount that you want.
+
+localStorage.setItem("playerSkin", 35); //Replace "skinyouwant" with the skin that you want, a number ranging from 1 to 35.
+
+localStorage.setItem("playerPet", 7); //Replace "petyouwant" with the pet that you want, a number ranging from 1 to 9.
+
+localStorage.setItem("playerPetLevel", 14); //Replace "levelyouwant" with the pet level that you want, a number ranging from 1 to 14.
+
+//These are the required xp levels that are needed to unlock certain islands. Use these with the playerXP command above.
+
+//0 - Tortuga
+//140 - Beach
+//700 - Easter
+//2100 - Wreck
+//4400 - Aztec
+//7600 - Volcano
+//13500 - Village

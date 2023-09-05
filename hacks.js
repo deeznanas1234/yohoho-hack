eaa3c8186dd97b74fa9a9d3302a81ca505f07dc6
deeznanas1234 <125383768+deeznanas1234@users.noreply.github.com>
2023-09-05 14:31:30 -0500

    Update hacks.js
    for u to be undefeated

--- hacks.js
+++ hacks.js
@@ -1,14 +1,14 @@
 //This is where the cheats are labeled and stored. Anytime you want to use them, use inspect element, click "console", copy/paste the command that you want to use, and reload the page.
 
-localStorage.setItem("coinsOwned", coinsyouwant); //Replace "coinsyouwant" with the amount that you want.
+localStorage.setItem("coinsOwned", 99999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999); //Replace "coinsyouwant" with the amount that you want.
 
-localStorage.setItem("playerXP", xpyouwant); //Replace "xpyouwant" with the amount that you want.
+localStorage.setItem("playerXP", 0); //Replace "xpyouwant" with the amount that you want.
 
-localStorage.setItem("playerSkin", skinyouwant); //Replace "skinyouwant" with the skin that you want, a number ranging from 1 to 35.
+localStorage.setItem("playerSkin", 35); //Replace "skinyouwant" with the skin that you want, a number ranging from 1 to 35.
 
-localStorage.setItem("playerPet", petyouwant); //Replace "petyouwant" with the pet that you want, a number ranging from 1 to 9.
+localStorage.setItem("playerPet", 7); //Replace "petyouwant" with the pet that you want, a number ranging from 1 to 9.
 
-localStorage.setItem("playerPetLevel", levelyouwant); //Replace "levelyouwant" with the pet level that you want, a number ranging from 1 to 14.
+localStorage.setItem("playerPetLevel", 14); //Replace "levelyouwant" with the pet level that you want, a number ranging from 1 to 14.
 
 //These are the required xp levels that are needed to unlock certain islands. Use these with the playerXP command above.
 
